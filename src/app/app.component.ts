43e0e3388702139b34bd0f2b39603be6b83928b8
joshdeveloper <joshdeveloper22@gmail.com>
2024-07-07 13:24:33 +0500

    Create core components and services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {MatButtonModule} from "@angular/material/button";
+import {
+
+    NavbarComponent}
+
+    from
+
+        "./core/components/navbar/navbar.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, MatButtonModule],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+                imports: [RouterOutlet, MatButtonModule, NavbarComponent],
+    template: `
+  <app-navbar />
+    `,
 })
-export class AppComponent {
-    title = 'rate-ideas';
+export
+
+class
+
+AppComponent {
+
 }
---
+++ src/app/core/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent {
+
+}
