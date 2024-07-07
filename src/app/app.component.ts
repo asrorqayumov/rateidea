db0eb3e98f5e12650b12a3748f30ab84c3c68cbb
joshdeveloper <joshdeveloper22@gmail.com>
2024-07-07 13:29:49 +0500

    Add pre-commit hook to format codes using Prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,12 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {MatButtonModule} from "@angular/material/button";
-import {
-
-    NavbarComponent}
-
-    from
-
-        "./core/components/navbar/navbar.component";
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-                imports: [RouterOutlet, MatButtonModule, NavbarComponent],
-    template: `
-  <app-navbar />
-    `,
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MatButtonModule, NavbarComponent],
+  template: ` <app-navbar /> `,
 })
-export
-
-class
-
-AppComponent {
-
-}
+export class AppComponent {}
