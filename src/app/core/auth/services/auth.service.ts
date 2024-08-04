08387c59040ec9b79e31b6fd3ee477717c33ba3c
Asror <qayumovasror2003@gmail.com>
2024-08-04 11:53:37 +0500

    post request to server implemented in login page

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -1,14 +1,22 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { SignUpFormValue } from '@core/models/iSignup';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
+import { LoginFormValue } from '@core/models/ILogin';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   http$ = inject(HttpClient);
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+  });
 
   signup(value: SignUpFormValue): Observable<string> {
     return this.http$.post<string>(environment.api + 'auth/sign-up', JSON.stringify(value));
+  }
+
+  login(value: LoginFormValue): Observable<string> {
+    return this.http$.post<string>(environment.api + 'auth/login', JSON.stringify(value), { headers: this.headers });
   }
 }
